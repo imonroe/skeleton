fcf9cd817be4c4ca250957fb989111376236d46d
Ian Monroe <ian@ianmonroe.com>
2018-04-29 18:04:36 -0500

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,12 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
-use imonroe\skeleton\Http\Controllers\SkeletonController;
+use :vendor\:package_name\Http\Controllers\SkeletonController;
 
-Route::namespace('imonroe\skeleton\Http\Controllers')->group(
+Route::namespace(':vendor\:package_name\Http\Controllers')->group(
     function () {
         Route::group(['middleware' => ['web']], function () {
-            Route::get('/skeleton/demo', 'SkeletonController@demo');
+            Route::get('/:package_name/demo', 'SkeletonController@demo');
         });
     }
 );
